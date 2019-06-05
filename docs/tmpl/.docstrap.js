f21d6414428c4f2b48bc2f3f8291d04775b9982f
Shim-Sao <35068017+shim-sao@users.noreply.github.com>
2019-06-05 12:10:17 +0200

    Update docs footer

--- docs/tmpl/.docstrap.js
+++ docs/tmpl/.docstrap.js
@@ -8,66 +8,20 @@
 const jsdocs = require("./.jsdocs.js");
 const pkg = require("../../package.json");
 
+const fs = require('fs');
+const path = require('path');
+
+const htmlFooter = fs.readFileSync(path.resolve(__dirname + "/docstrap/footer.html"));
+
+// console.log(htmlFooter);
+
 //jsdocs.opts.destination = "docs/build/jsdocs/inc-docstrap";
 jsdocs.opts.destination = "docs/html";
 jsdocs.templates.theme = "cosmo";
 jsdocs.templates.syntaxTheme  = "dark";
+jsdocs.templates.linenums = true;
 jsdocs.templates.logoFile = "img/logo.png";
-jsdocs.templates.footer = '<style>'
-+ '.m-2 {'
-+ '  margin:1rem;'
-+ '}'
-+ '.tutorial-section h2 {'
-+ '  display:none;'
-+ '}'
-+ '.tutorial-section header ul {'
-+ '  display:flex;'
-+ '  justify-content:center;'
-+ '  list-style:none'
-+ '}'
-+ '.tutorial-section header ul li {'
-+ '  display:flex;'
-+ '  padding:1rem;'
-+ '}'
-+ '.name h4 {'
-+ '  padding:1rem;'
-+ '  background:#2780E3;'
-+ '  color:#fff;'
-+ '}'
-+ 'h4.name {'
-+ '  padding:1rem;'
-+ '  background:#ff0fb4;'
-+ '  color:#fff;'
-+ '}'
-// + '.details, .description {'
-// + '  display:flex;'
-// + '  flex-wrap:wrap;'
-// + '}'
-// + '.details dd, .details dt {'
-// + '}'
-// + '.details dd, .details dt,'
-// + 'dd h5, dd ul {'
-// + '  display:flex;'
-// + '  flex-basis:50%;'
-// + '}'
-// + 'ul.dummy, .tag-see ul {'
-// + '  list-style:square;'
-// + '}'
-+ '</style>'
-+ '<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>'
-+ '<script>'
-+ '(adsbygoogle = window.adsbygoogle || []).push({'
-+ 'google_ad_client: "ca-pub-1175089473544116",'
-+ 'enable_page_level_ads: true'
-+ '});'
-+ '</script>'
-+ '<div class="container">'
-+ '<nav class="d-flex flex-row justify-content-center" style="text-align:center">'
-+ '<a class="btn btn-primary m-2" href="' + pkg.homepage + '" target="_blank">Repo</a>'
-+ '<a class="btn btn-primary m-2" href="' + pkg.bugs.url + '" target="_blank">Issues</a>'
-+ '<a class="btn btn-primary m-2" href="' + pkg.repository.url + '" target="_blank">Profil</a>'
-+ '<a class="btn btn-primary m-2" href="http://www.xtrmaddons.com/" target="_blank">Website</a>'
-+ '</nav>'
-+ '</div>';
+jsdocs.templates.search  = false;
+jsdocs.templates.footer = htmlFooter;
 
 module.exports = jsdocs;
